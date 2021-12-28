12c084fbded3c4ee7df61f9b72596bf69200b156
Seven <masterb21052017@gmail.com>
2021-12-28 23:10:14 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+$('.cross').click(() => {
+    $('.menu_container').attr("style", "display:none");
+})
+
+$('.navbar-toggler').click(() => {
+    $('.menu_container').attr("style", "display:block");
+})
